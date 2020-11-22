9907156084619c1d4d9995acbc18b252e3babf42
Ryan Shohoney <ryan.shohoney@gmail.com>
2020-11-22 07:59:41 -0600

    fix: Site metadata was set to gatsby defaults

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ryan and Amanda Shohoney`,
+    description: `Ryan and Amanda's Wedding website, welcome!`,
+    author: `Ryan Shohoney`,
+    siteUrl: 'https://shohoney.com'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
